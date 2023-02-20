f4e95c2b775a5f91b32c87d4802c469c6501127e
ubuntu-build <xu_yanfeng@126.com>
2023-02-20 12:05:31 +0000

    测试条件

--- compile-win.sh
+++ compile-win.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-# 这个脚本需要在MinGW32中运行
 # 1. 安装依赖
 ubuntu=1
 mingw32=2
 mingw64=3
-platform=${mingw32}
-     
-if (( ${platform} == ${ubuntu} )) then
-        # 在linux上
+platform=${ubuntu}
+
+if [ ${platform} -eq ${ubuntu} ]; then
+        echo "apt-get install ..."
         sudo apt-get update -qq
         sudo apt-get install -qqy \
                 git wget bzip2 file libwine-dev unzip libtool libtool-bin libltdl-dev pkg-config ant \
@@ -28,10 +27,10 @@ else
                 python3 python3-setuptools python3-mako \
                 gcc make ca-certificates nasm gnupg patch help2man \
                 python3 meson
-        if (( ${platform} == ${mingw32})) then
+        if [ ${platform} -eq ${mingw32} ]; then
                 echo "pacman dep mingw32 ..."
                 pacman -S --needed mingw32/mingw-w64-i686-ragel # 安装32位版本
-        elif (( ${platform} == ${mingw64})) then
+        elif [ ${platform} -eq ${mingw64} ]; then
                 echo "pacman dep mingw64 ..."
                 pacman -S --needed mingw64/mingw-w64-x86_64-ragel # 安装64位版本    
         fi
@@ -43,12 +42,13 @@ fi
 Exist=1
 UnExist=0
 state=${Exist}
-if (( ${platform} == ${ubuntu} )) then
-        if (( ${state} == ${UnExist}  )) then
+if [ ${platform} -eq ${ubuntu} ]; then
+        if [ ${state} -eq ${UnExist}  ]; then
                 wget https://github.com/mstorsjo/llvm-mingw/releases/download/20220906/llvm-mingw-20220906-msvcrt-ubuntu-18.04-x86_64.tar.xz
                 # 解压到了系统根目录下
                 tar xvf llvm-mingw-20220906-msvcrt-ubuntu-18.04-x86_64.tar.xz -C /opt
         fi
+        echo " install build tools ..."
         export PATH=/opt/llvm-mingw-20220906-msvcrt-ubuntu-18.04-x86_64/bin:$PATH
 else
         if (( ${state} == ${UnExist}  )) then
